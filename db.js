ac786725d830e6219b7704ff3be879471bc54cfe
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-11 21:44:43 +0530

    Sqlite Database Setup

--- db.js
+++ db.js
@@ -1,18 +1,31 @@
-var sqlite3 = require('sqlite3').verbose()
-const DBSOURCE = "db.sqlite"
+import sqlite3 from 'sqlite3'
+import 'dotenv/config'
 
-let db = new sqlite3.Database(DBSOURCE, (err)=>{
-    if (err){
-        console.error(err.message)
-    }else{
-        console.log('Connected to Database')
-        db.run(`
-        CREATE TABLE Todo(
-            id INTEGER PRIMARY KEY,
-            TASKS TEXT NOT NULL,
-            STATUS TEXT NOT NULL
-        )`)
+const DBSOURCE = (process.env.NODE_ENV === 'production' ? ':memory:' : './database.sqlite')
+
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        console.error('Database connection error:', err.message)
+    } else {
+        console.log('Connected to the SQLite database.')
     }
 })
 
-module.exports = db
+export const setUp = () => {
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS links (
+        id INTEGER PRIMARY KEY,
+        Tasks TEXT NOT NULL,
+        Status TEXT NOT NULL
+    )`
+    
+    db.run(createTableQuery, (err) => {
+        if (err) {
+            console.error('Error creating table:', err)
+        } else {
+            console.log('Table created successfully')
+        }
+    })
+}
+
+export const shutDown = () => db.close()
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import { setUp } from './db.js'
+
 const app = express()
+app.use(cors())
 const port = 3000
+
+setUp()
 
 app.get('/', (req, res) => {
     console.log('Here')
     res.send('Hi')
 })
-
 
 app.listen(port, ()=> {
     console.log(`Port: ${port}`)
