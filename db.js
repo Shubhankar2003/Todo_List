12976add3f80cd8ed6afbdf81f46d4ad63849d72
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-12 00:36:20 +0530

    Added query functions

--- db.js
+++ db.js
@@ -11,9 +11,53 @@ const db = new sqlite3.Database(DBSOURCE, (err) => {
     }
 })
 
+export const add_task = (task, status) => {
+    const insert_query = `
+    INSERT INTO Tasks (task, status) values (?, ?)
+    `;
+    db.run(insert_query, [task, status], function(err) {
+        if (err){
+            console.error('Error inserting task:', err);
+        }else{
+            console.log(`Task '${task}' added with ID ${this.lastID}`);
+        }
+    });
+};
+
+export const list_task = () => {
+    const select_query = `
+    SELECT * FROM Tasks
+    `;
+    db.all(select_query, [], function (err, rows){
+        if (err){
+            console.error('Error listing tasks:', err);
+        }else {
+            rows.forEach(row => {
+                console.log(`Task ID: ${row.id}, Task: ${row.Tasks}, Status: ${row.Status}`);
+            });
+        }
+    })
+}
+
+
+export const del_task = (taskId) =>{
+    const del_query = `
+    DELETE FROM Tasks WHERE id = ?
+    `;
+
+    db.run(del_query, [taskId], function (err){
+        if (err){
+            console.log('Error deleting task: ', err);
+        }else{
+            console.log(`Task with id ${taskId} deleted successfully`)
+        }
+    })
+}
+
+
 export const setUp = () => {
     const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS links (
+    CREATE TABLE IF NOT EXISTS Tasks (
         id INTEGER PRIMARY KEY,
         Tasks TEXT NOT NULL,
         Status TEXT NOT NULL
